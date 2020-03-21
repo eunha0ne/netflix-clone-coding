91acbdb41d61bea2ca0d78273c8d047d34309da2
eunha0ne <eunha0ne@gmail.com>
2020-03-21 21:04:01 +0900

    기본 구조 설계 (#1)

---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const API_KEY = "421771ed17b0da55803b567502a60f56";
+export const API_URL = `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`;
---
+++ src/layout/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <header>
+        <h1>React Movie Finder</h1>
+        <Navigation />
+      </header>
+      {children}
+      <footer>Created by @eunha0ne</footer>
+    </>
+  );
+};
+
+export default Layout;
