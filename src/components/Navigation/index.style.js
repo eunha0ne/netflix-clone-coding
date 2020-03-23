aa4658257b4c3a2775c46eeb6cf9a29e8c03c0f9
eunha0ne <eunha0ne@gmail.com>
2020-03-24 00:50:43 +0900

    컴포넌트 업데이트: KeyVisual (#5)

--- src/components/Navigation/index.style.js
+++ src/components/Navigation/index.style.js
@@ -1,3 +1,12 @@
 import styled from '@emotion/styled';
 
 export const Nav = styled.nav``;
+
+export const Ul = styled.ul`
+  display: flex;
+  flex-direction: row;
+
+  li {
+    padding-left: 1.6rem;
+  }
+`;
--- src/layout/index.js
+++ src/layout/index.js
@@ -9,7 +9,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      {children}
       <footer>Created by @eunha0ne</footer>
     </>
   );
