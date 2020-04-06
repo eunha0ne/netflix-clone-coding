b88a13eddecf67e2468a0c09e542e3f3e2be16f2
eunha0ne <eunha0ne@gmail.com>
2020-04-07 02:29:19 +0900

    슬라이드 컴포넌트 및 빌보드 피처 업데이트 (#21, #30)

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,7 @@ export const API_KEY = '421771ed17b0da55803b567502a60f56';
 export const BASE_URL = 'https://api.themoviedb.org/3';
 export const TV_IMG_URL = 'https://api.themoviedb.org/3/tv';
 export const BACK_IMG_URL = 'https://image.tmdb.org/t/p';
+export const SLIDE_PER_CARDS_LEN = 5;
 
 // poster   = Poster ............  500 x 750   2000 x 3000
 // backdrop = Fanart ............ 1280 x 720   3840 x 2160
