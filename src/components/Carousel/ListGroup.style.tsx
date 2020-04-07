3e1517c3a011d30d4fa9b44b312beec9f7244f72
eunha0ne <eunha0ne@gmail.com>
2020-04-07 15:29:26 +0900

    캐러셀 리팩토링 (#30)

---
+++ src/components/Carousel/ListGroup.style.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+interface UlProps {
+  pageIndex: number;
+}
+
+export const Ul = styled.ul`
+  z-index: 1;
+  position: relative;
+  white-space: nowrap;
+  transition: transform 0.54s cubic-bezier(0.5, 0, 0.1, 1) 0s;
+
+  ${({ pageIndex }: UlProps) => {
+    const shift = 100 * pageIndex;
+    return `transform: translate3d(-${shift}%, 0, 0);`;
+  }}
+`;
--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,91 +1,37 @@
-import React, { useState, useRef, useEffect, useMemo } from 'react';
-import { BACK_IMG_URL, SLIDE_PER_CARDS_LEN as CARDS_LEN } from '~/constants';
+import React, { useState } from 'react';
+import { SLIDE_PER_CARDS_LEN as CARDS_LEN } from '~/constants';
+import { IBillBoardProps } from '~/features/billBoard/types';
 
-import { InObserver, InObserverClosure } from '~/utils/intersectionObserver';
-import { IBillBoardProps, IMovie } from '~/features/billBoard/types';
-import blankPath from '~/assets/images/blank.png';
-
+import { ListGroup } from './ListGroup';
+import * as UI from '~/assets/ui/Icons';
 import * as S from './index.style';
 
 export const Carousel = (props: IBillBoardProps) => {
   const { movies } = props;
   const [pageIndex, setPageIndex] = useState(0);
 
-  const isAtStart = pageIndex > 0;
-  const isAtEnd = pageIndex + 1 < movies.length / CARDS_LEN;
-  const goPrev = () => isAtStart && setPageIndex(pageIndex - 1);
-  const goNext = () => isAtEnd && setPageIndex(pageIndex + 1);
+  const isNotStart = pageIndex > 0;
+  const isNotEnd = pageIndex + 1 < movies.length / CARDS_LEN;
+  const goPrev = () => isNotStart && setPageIndex(pageIndex - 1);
+  const goNext = () => isNotEnd && setPageIndex(pageIndex + 1);
 
   return (
     <S.Wrapper>
-      <S.Button arrowDir={'PREV'} onClick={goPrev}>
-        <span className="blind">이전 슬라이드</span>
-      </S.Button>
+      {isNotStart && (
+        <S.Button arrowDir="PREV" onClick={goPrev}>
+          <UI.ArrowLeft width="3.5vw" height="5vw" />
+          <span className="blind">이전 슬라이드</span>
+        </S.Button>
+      )}
 
       <ListGroup {...props} pageIndex={pageIndex} />
 
-      <S.Button arrowDir={'NEXT'} onClick={goNext}>
-        <span className="blind">다음 슬라이드</span>
-      </S.Button>
+      {isNotEnd && (
+        <S.Button arrowDir="NEXT" onClick={goNext}>
+          <UI.ArrowRight width="3.5vw" height="5vw" />
+          <span className="blind">다음 슬라이드</span>
+        </S.Button>
+      )}
     </S.Wrapper>
-  );
-};
-
-interface IListProps extends IBillBoardProps {
-  pageIndex: number;
-}
-
-const ListGroup = ({ pageIndex, movies, genre }: IListProps) => {
-  return (
-    <S.Ul pageIndex={pageIndex}>
-      {useMemo(() => {
-        return movies.map((movie, idx) => (
-          <ListItems
-            key={`${genre}-board-${idx}`}
-            movie={movie}
-            genre={genre}
-          />
-        ));
-      }, [movies])}
-    </S.Ul>
-  );
-};
-
-interface IListItem {
-  movie: IMovie;
-  genre: string;
-}
-
-const ListItems = ({ movie }: IListItem) => {
-  const itemEl = useRef<HTMLLIElement>(null);
-  const [imgPath, setImgPath] = useState(blankPath);
-
-  let iO: InObserverClosure;
-  const options = { threshold: 0.1 };
-  const func = () => {
-    const url = `${BACK_IMG_URL}/w500/${movie.poster_path}`;
-    setImgPath(url);
-    iO.disconnect();
-  };
-
-  useEffect(() => {
-    if (itemEl.current) {
-      iO = InObserver({
-        target: itemEl.current,
-        options: options,
-        callback: func
-      });
-
-      iO.observe();
-    }
-
-    return () => iO.disconnect();
-  }, [imgPath, itemEl.current, options]);
-
-  return (
-    <S.Li ref={itemEl}>
-      <strong>{movie.title}</strong>
-      <img src={imgPath} />
-    </S.Li>
   );
 };
