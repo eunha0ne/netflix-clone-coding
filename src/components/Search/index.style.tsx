225eeab44cac5b0a1cb6d3739af9e40363ef5208
eunha0ne <eunha0ne@gmail.com>
2020-04-10 01:31:17 +0900

    UI 컴포넌트 및 검색바 스타일 추가(#41, #47)

--- src/components/Search/index.style.tsx
+++ src/components/Search/index.style.tsx
@@ -1,3 +1,11 @@
 import styled from '@emotion/styled';
 
-export const Wrapper = styled.div``;
+export const Label = styled.label`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  svg {
+    margin-right: 1rem;
+  }
+`;
