49b0436c8db5601550b20ac871e11ac6a26b1101
eunha0ne <eunha0ne@gmail.com>
2020-04-22 14:05:34 +0900

    API 리팩토링 (#64)

--- src/api/movie.ts
+++ src/api/movie.ts
@@ -3,17 +3,24 @@ import axios from 'axios';
 import { IMovie, IResource } from '~/app/types';
 import { BASE_URL, API_KEY } from '~/constants';
 
+interface IParams {
+  api_key: string;
+  language: string;
+  query?: string;
+}
+
+let params: IParams = {
+  api_key: API_KEY,
+  language: 'ko'
+};
+
 export async function getMovie(
   resourcePath: string,
   uid: number
 ): Promise<IMovie> {
   const URL = `${BASE_URL}/${resourcePath}/${uid}`;
-  const params = {
-    api_key: API_KEY,
-    language: 'ko'
-  };
-
   const { data } = await axios.get(URL, { params });
+
   return data;
 }
 
@@ -41,40 +48,28 @@ export async function getMovies({
 
 export async function getGenres(mediaType: string = 'movie') {
   const URL = `${BASE_URL}/genre/${mediaType}/list`;
-  const params = {
-    api_key: API_KEY,
-    language: 'ko'
-  };
-
   const { data } = await axios.get(URL, { params });
+
   return data.genres;
 }
 
 export async function getCredits({ mediaType, id }: IResource) {
   const URL = `${BASE_URL}/${mediaType}/${id}/credits`;
-  const params = {
-    api_key: API_KEY,
-    language: 'ko'
-  };
-
   const { data } = await axios.get(URL, { params });
+
   return data.cast;
 }
 
 export async function getVideo({ mediaType, id }: IResource) {
   const URL = `${BASE_URL}/${mediaType}/${id}/videos`;
-  let params = {
-    api_key: API_KEY,
-    language: 'ko'
-  };
-
   const { data } = await axios.get(URL, { params });
   const isData = data.results > 0;
 
   if (isData) return data.results;
   else {
-    params = { ...params, language: `En-US` };
-    const { data } = await axios.get(URL, { params });
+    const { data } = await axios.get(URL, {
+      params: { ...params, language: `En-US` }
+    });
 
     return data.results;
   }
@@ -86,13 +81,9 @@ interface ISearch extends IResource {
 
 export async function getSearchMovie({ mediaType, id, keywords }: ISearch) {
   const URL = `${BASE_URL}/search/${mediaType}`;
-  let params = {
-    api_key: API_KEY,
-    language: 'ko',
-    query: keywords
-  };
+  const { data } = await axios.get(URL, {
+    params: { ...params, query: keywords }
+  });
 
-  const { data } = await axios.get(URL, { params });
-
-  console.log(data);
+  // console.log(data);
 }
