51f6fb8d32a64b03e6307153b864502cb9da426c
eunha0ne <eunha0ne@gmail.com>
2020-04-28 19:13:40 +0900

    검색 결과 컴포넌트, 반응형 개선 (#73)

--- src/components/ContentsBoard/Item.style.tsx
+++ src/components/ContentsBoard/Item.style.tsx
@@ -1,7 +1,6 @@
 import styled from '@emotion/styled';
 
 export const Li = styled.li`
-  /* margin-top: 3vw; */
   margin: 4px;
   width: calc(20% - 8px);
   box-sizing: border-box;
@@ -18,12 +17,19 @@ export const Li = styled.li`
 export const Button = styled.button`
   margin: 0;
   padding: 0;
+  padding-bottom: 53.333%;
+  position: relative;
   width: 100%;
+  height: 0;
+  overflow: hidden;
   border: none;
   background: none;
   font-size: 0;
 `;
 
 export const Img = styled.img`
+  position: absolute;
+  left: 0;
+  top: 0;
   width: 100%;
 `;
