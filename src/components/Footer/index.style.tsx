d95c9ca0132000094b1593ec109c3dd02316db68
eunha0ne <eunha0ne@gmail.com>
2020-04-29 13:03:25 +0900

    푸터 컴포넌트 및 스타일 생성 (#73)

---
+++ src/components/Footer/index.style.tsx
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export const Footer = styled.footer`
+  padding: 0 4%;
+  font-size: 1.4vh;
+`;
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import * as S from './index.style';
+
+export const Footer = () => {
+  return <S.Footer>Created by @eunha0ne</S.Footer>;
+};
