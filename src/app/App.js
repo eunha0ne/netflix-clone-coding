4216b9f6e3850ea19d5e6d03a9ff72fa35bb8212
eunha0ne <eunha0ne@gmail.com>
2020-05-03 17:31:53 +0900

    Update: 페이지 랜더링을 홈 컴포넌트에 위임 (#96)

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,10 @@
 import React, { Suspense, lazy } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import { Loading } from '~/components/Loading';
 import Layout from '~/layout';
 
 const Home = lazy(() => import('~/pages/Home'));
-const Tv = lazy(() => import('~/pages/Tv'));
-const Movie = lazy(() => import('~/pages/Movie'));
-const Latest = lazy(() => import('~/pages/Latest'));
 const MyList = lazy(() => import('~/pages/MyList'));
 const Search = lazy(() => import('~/pages/Search'));
 
@@ -17,16 +14,10 @@ export default function App() {
       <Suspense fallback={<Loading />}>
         <Switch>
           <Route exact path="/">
+            <Redirect to="/browse" />
+          </Route>
+          <Route path="/browse">
             <Home />
-          </Route>
-          <Route path="/tv">
-            <Tv />
-          </Route>
-          <Route path="/movie">
-            <Movie />
-          </Route>
-          <Route path="/latest">
-            <Latest />
           </Route>
           <Route path="/my-list">
             <MyList />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { KeyVisual } from '~/features/KeyVisual';
 import { PosterCard } from '~/features/PosterCard';
 import { Billboard } from '~/features/Billboard';
 
-import * as S from '~/assets/styles/main';
+import * as S from '~/assets/styles/Main';
+
+const defaultPageDef = {
+  menuName: 'home',
+  genre: 'movie',
+  resourcePath: '/trending/all/week',
+  sectionTitle: '특별 콘텐츠',
+  movieID: 0
+};
 
 const Home = () => {
-  const pageDefs = {
-    menuName: 'home',
-    genre: 'movie',
-    resourcePath: '/trending/all/week',
-    sectionTitle: '특별 콘텐츠'
-  };
+  const history = useHistory();
+
+  const [pageDef, setPageDef] = useState(defaultPageDef);
+
+  useEffect(() => {
+    console.log('/mounted', history);
+  }, []);
 
   return (
     <S.Main>
-      <KeyVisual {...pageDefs} uid={4935} />
-      <PosterCard {...pageDefs} />
-      <Billboard {...pageDefs} />
+      <KeyVisual {...pageDef} uid={4935} />
+      <PosterCard {...pageDef} />
+      <Billboard {...pageDef} />
     </S.Main>
   );
 };
