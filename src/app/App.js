b4db1d1ee9d6611aff4e5ccce993b44ae9d1f446
eunha0ne <eunha0ne@gmail.com>
2020-05-10 01:59:27 +0900

    Fix: 라우팅으로 인한 콘텐츠 출력 이슈 해결

--- src/app/App.js
+++ src/app/App.js
@@ -1,37 +1,47 @@
-import React, { Suspense, lazy } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import React, { Suspense, lazy, useEffect } from 'react';
+import { Route, Switch, Redirect, useLocation } from 'react-router-dom';
 
 import { Layout } from '~/layout';
 import { Loading } from '~/components/Loading';
 
-const Browse = lazy(() => import('~/pages/Browse'));
+const Home = lazy(() => import('~/pages/Browse'));
+const Tv = lazy(() => import('~/pages/Browse'));
+const Movie = lazy(() => import('~/pages/Browse'));
+const Latest = lazy(() => import('~/pages/Browse'));
 const MyList = lazy(() => import('~/pages/MyList'));
 const Search = lazy(() => import('~/pages/Search'));
 
 export default function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <Layout>
       <Suspense fallback={<Loading />}>
         <Switch>
           <Route exact path="/">
-            <Redirect to={'/browse'} />
+            <Redirect to="/browse" />
           </Route>
-          <Route exact path="/browse">
-            <Browse />
-          </Route>
+          <Route exact path="/browse" component={Home} />
           <Route
             path="/browse/:genre/:id"
             render={({ match }) => {
-              const isPathMatch = match.isExact;
-              return isPathMatch ? <Browse /> : <Redirect to={match.url} />;
+              if (match.isExact) {
+                const name = match.params.genre;
+                const page = {
+                  tv: <Tv />,
+                  movie: <Movie />,
+                  latest: <Latest />
+                };
+                return page[name];
+              } else return <Redirect to={match.url} />;
             }}
           />
-          <Route path="/my-list">
-            <MyList />
-          </Route>
-          <Route path="/search">
-            <Search />
-          </Route>
+          <Route path="/my-list" component={MyList} />
+          <Route path="/search" component={Search} />
           <Route path="*">
             <Redirect to="/browse" />
           </Route>
