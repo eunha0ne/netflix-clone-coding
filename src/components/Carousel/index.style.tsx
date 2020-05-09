ecb8687dd04c9758ec515b4c617a75c35e5ea331
eunha0ne <eunha0ne@gmail.com>
2020-05-09 19:41:02 +0900

    Refactor: 캐러셀 컴포넌트 스타일 리팩토링

--- src/components/Carousel/index.style.tsx
+++ src/components/Carousel/index.style.tsx
@@ -15,26 +15,24 @@ interface ButtonProps {
   arrowDir: 'PREV' | 'NEXT';
 }
 
-export const Button = styled.button`
+export const Button = styled.button<ButtonProps>`
   cursor: pointer;
   z-index: 10;
   position: absolute;
+  ${({ arrowDir }) => setLeft(arrowDir)}
   top: 0;
-  ${({ arrowDir }: ButtonProps) =>
-    arrowDir === 'PREV' ? 'left: 0;' : 'right: 0;'}
 
   width: 4%;
   height: 100%;
   box-sizing: border-box;
   border: none;
   background-color: rgba(0, 0, 0, 0.75);
-  transform: ${({ arrowDir }: ButtonProps) =>
+  transform: ${({ arrowDir }) =>
     arrowDir === 'PREV' ? 'translateX(-4%);' : 'translateX(4%);'};
 
   svg {
     position: absolute;
-    ${({ arrowDir }: ButtonProps) =>
-      arrowDir === 'PREV' ? 'left: 0;' : 'right: 0;'}
+    ${({ arrowDir }) => setLeft(arrowDir)}
     top: 50%;
     transform: translateY(-50%);
     color: white;
@@ -42,3 +40,6 @@ export const Button = styled.button`
     transition: opacity 300ms;
   }
 `;
+
+const setLeft = (arrowDir: string) =>
+  arrowDir === 'PREV' ? 'left: 0;' : 'right: 0;';
